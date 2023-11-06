acf098f40d8f041ef8ea8dd29979b2d5b5219c2b
Wize669 <william.zapata@epn.edu.ec>
2023-11-05 22:03:31 -0500

    feat: Add function getData in fetch file

--- getData.js
+++ getData.js
@@ -1,39 +1,51 @@
 let pin = {
-  pin: 3545,
+  pin: 2886,
 };
 
-async function sendPIN(pin) {
-  const url = "https://api-v2-node.onrender.com/api/v1/login";
-  console.log(pin);
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    //body: '{"pin":3545}'
-    body: JSON.stringify(pin),
-  };
+const url = "https://api-v2-node.onrender.com/api/v1/login";
 
-  try {
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      console.log("Error fetching the PIN");
+function sendPIN(url, pin) {
+  async function sendPINAPI(url, pin) {
+    console.log(pin);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      //body: '{"pin":3545}'
+      body: JSON.stringify(pin),
+    };
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        console.log("Error fetching the PIN");
+      }
+      const aux = await response.json();
+      //const { message } = aux;
+      return aux;
+    } catch (error) {
+      console.log("Error" + error);
     }
-    const aux = await response.json();
-    const { message } = aux;
-    console.log(message);
-  } catch (error) {
-    console.log("Error" + error);
   }
+
+  return sendPINAPI(url, pin)
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => {
+      console.log(error);
+      return null;
+    });
 }
 
-async function fetchAndPrintData(pin) {
+async function fetchAndPrintData(url, pin) {
   try {
-    const result = await sendPIN(pin);
+    const result = await sendPIN(url, pin);
     console.log(result);
   } catch (error) {
     console.error(error);
   }
 }
 
-fetchAndPrintData(pin);
+fetchAndPrintData(url, pin);
